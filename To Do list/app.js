2fab84779082dd0a4dbefc521307a7a87b5d9fd7
anwarmoazam <anwar.palimarwar@gmail.com>
2023-03-24 02:19:06 +0530

    project to-do

--- To Do list/app.js
+++ To Do list/app.js
@@ -1,4 +1,5 @@
 let listDOM = document.querySelector('.list-items');
+// let categoryDOM = document.getElementById('tasks');
 
 document.addEventListener("DOMContentLoaded", () => {
     const toDoList = init()
@@ -9,8 +10,9 @@ function init() {
     let toDoList = {};
     let defaultToDoList = {
         personal: {
-            housework: [{title:"Grocery",details:"",date:"",done:false}],
-            skincare: [{title:"Deep Exfoliation",details:"",done:false}],
+            housework: [{ title: "Cleaning", details: "Dish Cleaning", date: "", done: false },{ title: "Washing", details: "Cloths Washing", date: "", done: false }],
+            skincare: [{ title: "Deep Exfoliation", details: "", date:"", done: false }],
+            grocery: [{ title: "Cooking Oil", details: "15 Ltr", date:"", done: false },{ title: "Onion", details: "5 Kg", date:"", done: false }],
             meta: {
                 display_title: "Personal",
                 icon: `<span class="material-symbols-outlined personal">personal_injury</span>`,
@@ -18,20 +20,20 @@ function init() {
             }
         },
         work: {
-            programme: [{title:"IHIP Status",details:"Monthly Status of IHIP",date:"",done:false},
-                        {title:"Prepare SOE",details:"Prepare NVBDCP SOE",date:"",done:false}],
-            other: [{title:"Reports",details:"Prepare NHM Reports",date:"",done:false},
-                    {title:"Vidhan Sabha",details:"Reply of VSQ",date:"",done:false}],
+            programme: [{ title: "IHIP Status", details: "Monthly Status of IHIP", date: "", done: false },
+            { title: "Prepare SOE", details: "Prepare NVBDCP SOE", date: "", done: false }],
+            other: [{ title: "Reports", details: "Prepare NHM Reports", date: "", done: false },
+            { title: "Vidhan Sabha", details: "Reply of VSQ", date: "", done: false }],
             meta: {
-                display_title : "Work",
-                icon : `<span class="material-symbols-outlined work"> work </span>`,
+                display_title: "Work",
+                icon: `<span class="material-symbols-outlined work"> work </span>`,
                 color: "red"
             }
         },
         study: {
-            lecture: [{title:"Java Script",details:"Complete Will Sentance Lecture",date:"",done:false}],
-            code : [{title:"Java Script",details:"Complete Will Sentance Lecture Code",date:"",done:false}],
-            meta:{
+            lecture: [{ title: "Java Script", details: "Complete Will Sentance Lecture", date: "", done: false }],
+            code: [{ title: "Java Script", details: "Complete Will Sentance Lecture Code", date: "", done: false }],
+            meta: {
                 display_title: "Study",
                 icon: `<span class="material-symbols-outlined lecture">play_lesson</span>`,
                 color: "aqua"
@@ -47,10 +49,10 @@ function init() {
     return toDoList;
 }
 
-function renderCategoryList(categoryList){
+function renderCategoryList(categoryList) {
     // let categoryListDOM = "";
     let fragment = document.createDocumentFragment();
-    for(let category in categoryList){
+    for (let category in categoryList) {
         let listItem = document.createElement('div');
         listItem.classList.add('options');
         listItem.innerHTML = `${categoryList[category].meta.icon}${categoryList[category].meta.display_title}`
@@ -59,12 +61,19 @@ function renderCategoryList(categoryList){
     }
     // listDOM.innerHTML = categoryListDOM;
     listDOM.appendChild(fragment);
+    
+    
     let listItemsDOM = listDOM.querySelectorAll('.options');
     listItemsDOM.forEach(item => {
-        item.addEventListener('click',function(e){
-            console.log(e.currentTarget);
+        item.addEventListener('click', function (e) {
+            let text = e.currentTarget.innerText.split("\n");
+            text = text[text.length - 1].trim();
+            text = text.toLowerCase();
+            const {[text] : currentCategoryList} = init();
+            for(let listItem in currentCategoryList){
+                console.log(listItem,currentCategoryList[listItem]);
+            }
         });
     })
-    
-    
 }
+
